b3525b4a9d2a0d53be6648a6ad7446edd70347d5
quicksoftsolutions <41007763+quicksoftsolutions@users.noreply.github.com>
2020-09-01 14:34:01 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,270 @@
+<!doctype html>
+<html class="no-js" lang="">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>Functional Foods || Unlocking the Medicine in Foods</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<!-- favicon
+		============================================ -->		
+        <link rel="shortcut icon" type="image/x-icon" href="img/farvion.png">
+		
+		<!-- Google Fonts
+		============================================ -->		
+		<link href='https://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+		<link href="http://allfont.net/allfont.css?fonts=montserrat-light" rel="stylesheet" type="text/css" />
+		<!--linearicons font-->
+		<link rel="stylesheet" href="css/linearfont.css">
+		<!-- Bootstrap CSS
+		============================================ -->		
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+		<!-- meanmenu CSS
+		============================================ -->		
+        <link rel="stylesheet" href="css/meanmenu.min.css">
+		<!-- Bootstrap CSS
+		============================================ -->
+        <link rel="stylesheet" href="css/font-awesome.min.css">
+		<!-- owl.carousel CSS
+		============================================ -->
+        <link rel="stylesheet" href="css/owl.carousel.css">
+        <link rel="stylesheet" href="css/owl.theme.css">
+        <link rel="stylesheet" href="css/owl.transitions.css">
+		<!-- animate CSS
+		============================================ -->
+        <link rel="stylesheet" href="css/animate.css">
+		<!-- normalize CSS
+		============================================ -->
+        <link rel="stylesheet" href="css/normalize.css">
+		<!-- Nivo Slider CSS -->
+		<link rel="stylesheet" href="css/nivo-slider.css">
+		<!-- Add venobox css -->
+		<link rel="stylesheet" href="venobox/venobox.css"> 
+		<!-- main CSS
+		============================================ -->
+        <link rel="stylesheet" href="css/main.css">
+		<!-- Nivo Slider CSS -->
+		<link rel="stylesheet" href="css/nivo-slider.css">		
+		<!-- style CSS
+		============================================ -->
+        <link rel="stylesheet" href="style.css">
+		<!-- responsive CSS
+		============================================ -->
+        <link rel="stylesheet" href="css/responsive.css">
+		<!-- modernizr JS
+		============================================ -->		
+        <script src="js/vendor/modernizr-2.8.3.min.js"></script>
+    </head>
+    <body>
+        <!--[if lt IE 8]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+	<!--start header  area --> 
+	<div class="header_area">
+		<div class="container">
+			<div class="row">
+				<!-- header  logo --> 
+				<div class="col-md-4 col-sm-3 col-xs-12">
+					<div class="logo"><a href="index.php "><img src="img/logo.png" alt="" /></a></div>
+				</div>
+				<!-- end header  logo --> 
+				<div class="col-md-8 col-sm-9 col-xs-12">
+					<div>
+						<div class="social_icon pull-right">
+							<p>
+							   <a target="_blank" href="#" class="icon-set"><i class="fa fa-facebook"></i></a> 
+							   <a target="_blank" href="#" class="icon-set"><i class="fa fa-twitter"></i></a> 
+							   <a target="_blank" href="#" class="icon-set"><i class="fa fa-linkedin"></i></a>
+							</p>
+						</div>				
+					</div>
+					<div class="phone_address pull-right clear">
+						<p class="no-margin">
+						  <small>
+							  <span class="text-msg">Have any questions?</span>
+							  <span class="icon-set"><i class="fa fa-phone"></i> +234 1973 833 508</span> 
+							  <span class="icon-set"><i class="fa fa-envelope"></i> goboh@futa.edu.ng</span> 
+							  <span class="icon-set"><i class="fa fa-linkdnln"></i>Profile</span> 
+						  </small>
+						</p>				
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+    <!--end header  area --> 
+    <!--Start nav  area --> 
+	<div class="nav_area">
+		<div class="container">
+			<div class="row">
+				<!--nav item-->
+				<div class="col-md-12 col-sm-12 col-xs-12">
+					<!--  nav menu-->
+					<nav class="menu">
+						<ul class="navid pull-left">
+							<li><a href="https://www.futa.edu.ng" target="_blank">FUTA Website </a></li>
+							
+						</ul>
+					</nav>
+					<!--end  nav menu-->	
+					
+				</div>
+				<!--end nav item -->
+			</div>	
+		</div>
+	</div>
+	<!--end nav  area -->
+	<!--Start mobile menu  area -->
+	<div class="mobile_memu_area">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12 col-md-12 col-sm-12">
+					<div class="mobile_memu">
+					<!--  nav menu-->
+					<nav>
+						<ul class="navid">
+							<li><a href="https://www.futa.edu.ng">Home</a></li>
+						</ul>
+					</nav>
+					<!--end  nav menu-->
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!--end mobile menu  area -->	
+    <!--Start about title  area --> 
+	<div class="about_area_s" style="background-image: url(img/about.jpg);">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<div class="heading_about">
+						<h1 class="page-title">
+						<small> </small>
+							
+						</h1>					
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+    <!--Start about  area --> 
+	<div class="about_area page">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<div class="title">
+						<h3 class="module-title about_titlea">
+						&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+						  <span>Functional Foods </span>
+						</h3>
+					</div>
+				</div>
+			</div>		
+			<div class="row">
+				<div class="col-md-6 col-sm-6">
+					<div class="content-inner">
+						<div class="content-desc">
+							<div class="">
+								<p class="about_pra_top">Unlocking the Medicine in Foods</p>
+							</div>
+
+							<div class="">
+								<p>My books have been written on foods and their nutritive values while even many more exist on health and wellness. However, M
+								the connection between food and health is often glossed over or at best limited to the consumption of some foods as risk factors for some diseases. However, while it's true that some foods can kill, it is also true that some foods can heal- these are called functional foods.  </p>
+								
+								<p>This  book  focuses  on  the  concept  of  functional  food  as  key  to unlocking  the  medicine  in  food.  It  has  been  richly  packed  with  various topics  that  has  been  carefully  divided  into  sections  including,  role  of functional foods in optimizing health care delivery in developing world and mechanisms  of  action  of  food  bioactive  components.  The  aspect  of Consumer  acceptability  of  functional  foods  and  nutraceuticals  was  also well  considered  in  this  book,  as  well  as  increasing  awareness  on biodiversity and conservation of plant and animal resources as functional foods and nutraceuticals. </p>
+
+								<p>In view of the fact that the field of functional food research is gradually evolving and lots of technological advancements are being  experienced  in  order  to  foster  developments  of  functional  food products  at  industrial  scale,  this  book  also  contain  sections  on biotechnology as a vital tool for bridging the gap between functional food research  and  industrial  utilization,  and  functional  food  product development  and regulations. </p>
+
+								<p>The  aspect of  nutrigenomics  which  is  the emerging field of 'personalized nutritional therapy' was not also left out. Therefore, this book offers a lot for everyone including the nutritionists, biochemists,  food  scientists,  biotechnologists,  agriculturists, conservationist, entrepreneurs, industrialists and policy makers </p>
+
+								
+							</div>
+						  </div>
+					</div>
+				</div>
+				<div class="col-md-6 col-sm-6">
+					<div class="ab_thumb">
+						<img src="img/about1.jpg" alt="" class="img-thumbnail" />
+					  </div>				
+				</div>
+				<div class="col-md-12 col-sm-12">
+					<div class="about_bottom_text">
+						<p>This  book  is  dedicated  to  Alexander  von  Humboldt foundation</p>
+						<div class="text-center"><a href="#" class="readmore about_more">This  book  is  dedicated  to  Alexander  von  Humboldt foundation</a></div>
+					</div>
+				</div>				
+			</div>
+		</div>
+	</div>
+	<!--end about  area -->	
+
+
+	
+	<!-- breadcrumb-area start -->
+	<div class="breadcrumb-area">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<div class="breadcrumb">
+						<ul>
+							<li><a href="http://www.futa.edu.ng">FUTA Website</a> <i class="fa fa-angle-right"></i></li>							
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- breadcrumb-area end -->	
+	
+	<!-- footer bottom area -->
+	<div class="footer_bottom_area">
+		<div class="container">
+			<div class="row">
+				<div class=" col-sm-6 col-md-6 col-lg-6">
+					<div class="footer_text">
+						<p>
+							Copyright © 2020 Food Function. All Rights Reserved.
+						</p>
+					</div>
+				</div>
+				<div class=" col-sm-6 col-md-6 col-lg-6">
+					<p class="text-right">Design By <a href="http://www.crc.futa.edu.ng" target="_blank">FUTA CRC</a></p>
+				</div>				
+			</div>
+		</div>	
+	</div>		
+		<!-- jquery
+		============================================ -->		
+        <script src="js/vendor/jquery-1.11.3.min.js"></script>
+		<!-- bootstrap JS
+		============================================ -->		
+        <script src="js/bootstrap.min.js"></script>
+		<!-- wow JS
+		============================================ -->		
+        <script src="js/wow.min.js"></script>
+		 <!-- Nivo Slider JS -->
+		<script src="js/jquery.nivo.slider.pack.js"></script> 		
+		<!-- meanmenu JS
+		============================================ -->		
+        <script src="js/jquery.meanmenu.min.js"></script>
+		<!-- owl.carousel JS
+		============================================ -->		
+        <script src="js/owl.carousel.min.js"></script>
+		<!-- scrollUp JS
+		============================================ -->		
+        <script src="js/jquery.scrollUp.min.js"></script>
+		<!-- Apple TV Effect -->
+        <script src="js/atvImg-min.js"></script>
+		<!-- Add venobox js -->
+		<script type="text/javascript" src="venobox/venobox.min.js"></script>
+		<!-- plugins JS
+		============================================ -->		
+        <script src="js/plugins.js"></script>
+		<!-- main JS
+		============================================ -->		
+        <script src="js/main.js"></script>
+    </body>
+</html>
